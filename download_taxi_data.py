ddef39c223f43d0f3a0db061941d03f241304ad9
jon.lopez <jonathan.lopez@gridunity.com>
2022-02-18 14:29:46 -0500

    Refactored script for increased readability

--- download_taxi_data.py
+++ download_taxi_data.py
@@ -1,8 +1,8 @@
 import asyncio
-import itertools as it
+from itertools import product, starmap
 from typing import NamedTuple
 
-from aiohttp import ClientConnectionError, ClientSession, ClientTimeout, request
+from aiohttp import ClientConnectionError, ClientTimeout, request
 from aiomultiprocess import Pool
 from tqdm import tqdm
 
@@ -10,74 +10,108 @@ BASE_URL: str = "https://s3.amazonaws.com/nyc-tlc/trip+data"
 CHUNK_SIZE = 10 * 1_000_000  # TODO test with different sized chunks
 
 
+# TODO Better class name?
 class MetaData(NamedTuple):
+    """Generalized container class to pass data between functions"""
+
     path: str
     url: str
     size: int
 
 
-async def check_url(file_path: str) -> MetaData | None:
-    """Ping file url to verify exisitance and grab size data"""
+def generate_file_path(taxi: str, year: str, month: str) -> str:
+    """Generate target file path"""
+    return f"{taxi}_tripdata_{year}-{month}.csv"
+
+
+def generate_candidate_file_paths() -> list[str]:
+    """Generate candidate file paths from NYC TLC Trip record data:
+    https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+    """
+    taxis: list[str] = ["green", "yellow", "fhv", "fhvhv"]
+    taxis: list[str] = ["fhv"]  # NOTE remove when downloading all files
+    years: list[str] = [str(year) for year in range(2009, 2022)]
+    months: list[str] = [f"{month:02}" for month in range(1, 13)]
+
+    return list(starmap(generate_file_path, product(taxis, years, months)))
+
+
+async def validate_endpoint(file_path: str) -> MetaData | None:
+    """Ping file url to validate endpoint"""
     try:
         url = f"{BASE_URL}/{file_path}"
-        async with request("GET", url) as resp:
-            if resp.status == 200 and resp.content_length is not None:
-                return MetaData(file_path, url, resp.content_length)
+        async with request("GET", url) as response:
+            if response.status == 200 and response.content_length is not None:
+                return MetaData(file_path, url, response.content_length)
     except ClientConnectionError:
         return None
 
 
-async def download_file(metadata: MetaData):
+async def validate_candidate_endpoints() -> list[MetaData]:
+    """ """
+
+    candidate_file_paths: list[str] = generate_candidate_file_paths()
+
+    # Asynchronously and concurrenctly ping each candidate url
+    valid_results: list[MetaData] = []
+
+    pbar = tqdm(
+        total=len(candidate_file_paths),
+        position=0,
+        desc=f"Validating {len(candidate_file_paths)} endpoints: ",
+    )
+    async with Pool() as p:
+        async for call in p.map(validate_endpoint, candidate_file_paths):
+            pbar.update(1)
+            if call:
+                valid_results.append(call)
+    print(f"{len(valid_results)} files found")
+    return valid_results
+
+
+async def download_file(metadata: MetaData) -> int:
     """Given a verified file url, download the target file in chunks"""
-    async with request(
-        "GET", metadata.url, timeout=ClientTimeout(total=100 * 60)
-    ) as resp:
-        # TODO introduce error handling instead of aborting
-        if resp.status != 200:
-            print(f"{metadata.path} not valid")
-            return
+    try:
+        # HACK increased timeout for large files, is there a better way?
+        async with request(
+            "GET", metadata.url, timeout=ClientTimeout(total=100 * 60)
+        ) as response:
 
-        # TODO consider changing downloaded file path
-        with open(f"data/{metadata.path}", "wb") as fd:
-            with tqdm(total=metadata.size) as pbar:
-                async for chunk in resp.content.iter_chunked(CHUNK_SIZE):
-                    pbar.update(fd.write(chunk))
-        print(f"{metadata.path} downloaded!")
-
-
-def generate_file_url(taxi: str, year: str, month: str) -> str:
-    """Generate target file url"""
-    return f"{taxi}_tripdata_{year}-{month}.csv"
+            # TODO consider changing downloaded file path
+            with open(f"data/{metadata.path}", "wb") as file_dump, tqdm(
+                total=metadata.size,
+                desc=metadata.path,
+                unit_scale=True,
+                # bar_format="{lbar}{r_bar}",
+                # position=8,
+                leave=True,
+            ) as pbar:
+                async for chunk in response.content.iter_chunked(CHUNK_SIZE):
+                    pbar.update(file_dump.write(chunk))
+    except ClientConnectionError:
+        pass
+    return metadata.size
 
 
 async def download_files(metadata: list[MetaData]) -> None:
-    # TODO experiment with pool parameters
-    async with Pool(
-        processes=2, maxtasksperchild=8, queuecount=2, childconcurrency=4
-    ) as pool:
-        _ = await pool.map(download_file, metadata)
-
-
-async def collect_valid_urls() -> list[MetaData]:
-    # taxis: list[str] = ["green", "yellow", "fhv", "fhvhv"]
-    taxis: list[str] = ["fhv"]
-    years: list[str] = [str(year) for year in range(2009, 2022)]
-    months: list[str] = [f"{month:02}" for month in range(1, 13)]
-
-    file_urls: list[str] = list(
-        it.starmap(generate_file_url, it.product(taxis, years, months))
+    """Asynchronously and concurrenctly download all files"""
+    total_files_pbar = tqdm(total=len(metadata), position=1, desc="File Count")
+    total_size_pbar = tqdm(
+        total=sum(x.size for x in metadata),
+        position=2,
+        desc="Downloaded",
+        unit_scale=True,
     )
-    result: list[MetaData] = []
-    async with Pool() as pool:
-        with tqdm(total=len(file_urls)) as pbar:
-            async for mapping in pool.map(check_url, file_urls):
-                pbar.update(1)
-                if mapping:
-                    result.append(mapping)
-    return result
+
+    # PERF experiment with parameters
+    async with Pool(
+        processes=2, maxtasksperchild=4, queuecount=2, childconcurrency=4
+    ) as pool:
+        async for downloaded in pool.map(download_file, metadata):
+            total_files_pbar.update(1)
+            total_size_pbar.update(downloaded)
 
 
 if __name__ == "__main__":
-    valid_url_metadata: list[MetaData] = asyncio.run(collect_valid_urls())
-    print(f"{len(valid_url_metadata)} files found")
+    valid_url_metadata = asyncio.run(validate_candidate_endpoints())
     asyncio.run(download_files(valid_url_metadata))
