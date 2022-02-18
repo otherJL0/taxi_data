13d5acd4ea4ad04f547df368d9af39b57e5dbcd6
jon.lopez <jonathan.lopez@gridunity.com>
2022-02-18 10:09:10 -0500

    Refactor function to check for valid urls

--- download_taxi_data.py
+++ download_taxi_data.py
@@ -2,7 +2,6 @@ import asyncio
 
 # import aioitertools.itertools as it
 import itertools as it
-import sys
 
 from aiohttp import ClientTimeout, request
 from aiomultiprocess import Pool
@@ -11,11 +10,11 @@ BASE_URL: str = "https://s3.amazonaws.com/nyc-tlc/trip+data"
 CHUNK_SIZE = 10 * 1_000_000
 
 
-async def check_url(file_name: str) -> str:
+async def check_url(taxi: str, year: str, month: str) -> dict[str, int] | None:
+    file_name = f"{taxi}_tripdata_{year}-{month}.csv"
     async with request("GET", f"{BASE_URL}/{file_name}") as resp:
         if resp.status == 200 and resp.content_length is not None:
-            return file_name
-        return ""
+            return {file_name: resp.content_length}
 
 
 async def download_file(file_name: str):
@@ -35,24 +34,26 @@ def generate_file_name(taxi: str, year: str, month: str) -> str:
     return f"{taxi}_tripdata_{year}-{month}.csv"
 
 
-async def main(files: list[str]) -> None:
+async def download_files(files: list[str]) -> None:
     async with Pool() as pool:
         _ = await pool.map(download_file, files)
 
 
-async def calculate_total_size(taxi: str, year: int) -> list[str]:
-    taxis: list[str] = [taxi]
-    years: list[str] = [str(year) for year in range(year, year + 1)]
+async def calculate_total_size() -> dict[str, int]:
+    taxis: list[str] = ["green", "yellow", "fh", "fhvhv"]
+    years: list[str] = [str(year) for year in range(2009, 2022)]
     months: list[str] = [f"{month:02}" for month in range(1, 13)]
 
-    files = it.starmap(generate_file_name, it.product(taxis, years, months))
+    result: dict[str, int] = {}
     async with Pool() as pool:
-        files = await pool.map(check_url, files)
-        return [x for x in files if x]
+        combinations = list(it.product(taxis, years, months))
+        async for mapping in pool.starmap(check_url, combinations):
+            if mapping:
+                result = result | mapping
+    return result
 
 
 if __name__ == "__main__":
-    taxi = sys.argv[1]
-    year = int(sys.argv[2])
-    valid_files = asyncio.run(calculate_total_size(taxi, year))
-    asyncio.run(main(valid_files))
+    valid_files = asyncio.run(calculate_total_size())
+    print(valid_files)
+    # asyncio.run(download_files(valid_files))
